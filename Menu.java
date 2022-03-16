d50b94de68db86f57dc75dd34f4d494a624b2581
Cwalter17 <waltercody11@yahoo.com>
2022-03-15 18:18:26 -0700

    menu

---
+++ Menu.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Menu {
+    private MenuItem[] items;
+
+    public void loadFromFile(String file){
+
+    }
+
+    public void storeToFile(String file){
+
+    }
+
+    public void addItem(MenuItem name){
+
+    }
+
+    public void removeItem(MenuItem name){
+
+    }
+
+    public MenuItem[] getItems(){
+        return MenuItem;
+    }
+
+}
