ba1a198f5e7dd7a73d567e1dd8c050dc33f0dc6d
danielstuff <91523424+danielstuff@users.noreply.github.com>
2022-03-30 11:27:39 -0700

    Add files via upload
    updating files tomake sure everything is up to date

---
+++ src/main/java/README.md
@@ -0,0 +1,9 @@
+### TeamProject
+This is the GitHub repo for our team project for CSE360. The team project is a demo program for an online ordering platform for a restuarant. It will have functionality for customers and empoyees of the restuarant.
+<hr>
+Team Members:<br/>
+Sam Kacprowicz <br/>
+Cody Walter <br/>
+Eric Wu <br/>
+Ian McDonough <br/>
+Daniel Cohen <br/>
---
+++ src/main/java/src/main/java/LoginUI.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author daniel
+ * 
+ */
+import javafx.scene.layout.Pane;
+public class LoginUI extends Pane {
+
+    private POSApplication _posApp;
+
+    public LoginUI(POSApplication app) {
+
+        _posApp = app;
+
+    }
+
+    private void showMainScreen() {
+
+    }
+
+    private void showAccountCreation() {
+
+    }
+
+    public void startLogin() {
+
+    }
+}
