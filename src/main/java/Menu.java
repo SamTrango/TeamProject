7d9622f28f29f9ee9afcc98ff70d16fa24f42872
Cwalter17 <waltercody11@yahoo.com>
2022-03-31 19:14:29 -0700

    Hopefully these work lol

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,23 +1,61 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.jar.Attributes.Name;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 public class Menu {
     private ArrayList<MenuItem> items;
+    String line;
+    int reading = 0;
+    int itemCount = 0;
+    int ingredientCount = 0;
+    String temp;
+    public void loadFromFile(String file) throws FileNotFoundException{ 
+    Scanner read = new Scanner (new File(file)); 
+    read.useDelimiter(",");
+            String Name, imageSrc;
+            ArrayList<String> ingredients = new ArrayList<String>();
+            int price, prepareTime;
+            for(int i = 0; i < itemCount;i++){
 
-    public void loadFromFile(String file){
+            while (read.hasNext()) //order of file is price, name, preptime, image source then ingredients
+            {
+               price = read.nextInt();
+               Name = read.nextLine();
+               imageSrc = read.next();
+               prepareTime = read.nextInt();
+               temp = read.nextLine();
+               while(temp != null){
+                   ingredients.add(ingredientCount,temp.substring(0, temp.indexOf(",")));
+                   temp = temp.substring(temp.indexOf(","));
+                   ingredientCount++;
+               }
+               MenuItem item = new MenuItem(Name,ingredients,imageSrc,price,prepareTime);
+               addItem(item);
+            }
+
+        }
 
     }
-
-    public void storeToFile(String file){
-
+    public void storeToFile(String file) throws IOException{
+        FileWriter fw = new FileWriter(file);
+        for(int i = 0; i < items.size();i++){
+            fw.write((int) items.get(i).getPrice());
+            fw.write(items.get(i).getName());
+            fw.write(items.get(i).getImgSrc());
+            fw.write(items.get(i).getPrepTime());
+            fw.write(items.get(i).getIngredients().toString());
+        }
     }
     public void addItem(MenuItem name){        
-
+        items.add(name);
     }
-
     public void removeItem(MenuItem name){
-
+        items.remove(name);
     }
-
     public List<MenuItem> getItems(){
         return items;
     }
--- src/main/java/MenuItem.java
+++ src/main/java/MenuItem.java
@@ -1,17 +1,27 @@
 import java.util.ArrayList;
-import java.util.List;
 public class MenuItem {
     public String name;
     private ArrayList<String> ingredients;
     private String imageSrc;
     private double price;
     private int prepareTime;
+    
+    public MenuItem(String name, ArrayList<String> ingredients, String imageSrc, double price, int prepareTime) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.imageSrc = imageSrc;
+        this.price = price;
+        this.prepareTime = prepareTime;
+    }
 
+    public int getPrepTime(){
+        return prepareTime;
+    }
     public String getName(){
         return name;
     }
-    public List<String> getIngredients(){
-        return ingredients;
+    public String[] getIngredients(){
+        return (String[]) ingredients.toArray();
     }
     public String getImgSrc(){
         return imageSrc;
@@ -20,15 +30,15 @@ public class MenuItem {
         return price;
     }
     public void setName(String name){
-
+        this.name = name;
     }
-    public void setIngredients(String[] ingredients){
-
+    public void setIngredients(ArrayList<String> ingredients){
+        this.ingredients = ingredients;
     }
     public void setImgSrc(String source){
-
+        this.imageSrc = source;
     }
     public void setPrice(double price){
-
+        this.price = price;
     }
 }
